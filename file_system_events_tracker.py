3fb72e3487e6e1e190d435a7b212a70a966ceae6
ranadheer2012 <110771454+ranadheer2012@users.noreply.github.com>
2023-08-15 19:19:49 +0530

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,64 @@
+import sys
+import time
+import random
+
+import os
+import shutil
+import logging
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "/Users/Kuttimma/Downloads"
+
+# Event Hanlder Class
+class FileEventHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        print(f"Hey, {event.src_path} has been created!")
+
+    def on_created(self, event): 
+        print(f"Opps someone deleted! {event.src_path}")
+
+    def on_created(self, event): 
+        print(f"Hey there, {event.src_path} has been modified!")
+
+    def on_created(self, event):
+        print(f"someone moved {event.src_path} to {event.dest_path}")
+
+    #1_on_created
+
+    #2_on_deleted
+
+    #3_on_modified
+
+    #4_on_moved
+
+        
+
+
+# Initialize Event Handler Class
+event_handler = FileEventHandler()
+
+# Initialize Observer
+observer = Observer()
+
+# Schedule the Observer
+observer.schedule(event_handler, from_dir, recursive=True)
+
+
+# Start the Observer
+observer.start()
+
+
+#5_Write a exception for keyboardInterrupt
+
+while True:
+    time.sleep(2)
+    print("running...")
+
+
+
+
+
+
